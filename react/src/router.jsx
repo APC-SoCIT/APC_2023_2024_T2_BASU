01022c441fd29964a3c0eac8f26027c48d4ec7a9
Zel <hbernante@student.apc.edu.ph>
2023-12-28 21:25:24 +0800

    Default Layout Routing for Dashboard and Location Tracking

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -4,16 +4,25 @@ import Login from "./views/Login";
 import LocationTracking from "./views/LocationTracking";
 import Dashboard from "./views/Dashboard";
 import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 const router = createBrowserRouter([
+
     {
         path: '/',
-        element: <Dashboard />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/locationtracking',
+                element: <LocationTracking />
+            },
+        ]
     },
-    {
-        path: '/locationtracking',
-        element: <LocationTracking />
-    },
+
     {
         path: '/',
         element: <GuestLayout />,
@@ -27,7 +36,7 @@ const router = createBrowserRouter([
                 element: <Signup />
             },
         ]
-    },
+    }
 
 ])
 
