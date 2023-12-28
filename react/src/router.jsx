492a18af2228511e9b6792aa647c0c405a76d4ea
Zel <hbernante@student.apc.edu.ph>
2023-12-28 13:28:06 +0800

    Login Interface

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,10 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Signup from "./views/Signup";
+import Login from "./views/Login";
+import LocationTracking from "./views/LocationTracking";
+import Dashboard from "./views/Dashboard";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
+        element: <Dashboard />
+    },
+    {
+        path: '/locationtracking',
+        element: <LocationTracking />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/signup',
+        element: <Signup/>
     }
 ])
 
