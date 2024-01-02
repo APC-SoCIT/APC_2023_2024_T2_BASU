636a974cd8ab9c1facf3ed1fd411a76a89beac85
Zel <hbernante@student.apc.edu.ph>
2024-01-03 00:32:41 +0800

    Backend Routing for Account Registration

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,14 +1,101 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios";
+
 export default function Signup() {
+  const [fullName, setFullName] = useState('');
+  // const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState({__html: ''});
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    setError({__html: ''})
+
+    axiosClient.post('/signup', {
+      name: fullName,
+      email,
+      password,
+      password_confirmation: passwordConfirmation
+    })
+    .then(({ data }) => {
+      console.log(data);
+    })
+    .catch(({ error }) => {
+      if (error.response) {
+        const finalErrors = Object.values(error.response.data.errors).reduce((accum, next) => [...accum, ...next], [])
+        console.log(finalErrors)
+        setError({__html: finalErrors.join('<br>')})
+      }
+      console.error(error)
+    });
+
+  };
+
   return (
     <>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign-Up Here
           </h2>
-          <form className="space-y-6" action="#" method="POST">
+          <form onSubmit={onSubmit} className="space-y-6" action="#" method="POST">
+
+          {error.__html && (<div className="bg-red-500 rounded py-2 px-3 text-white" dangerouslySetInnerHTML={error}>
+          </div>)}
+
+          <div>
+              <label htmlFor="full-name" className="block text-sm font-medium leading-6 text-gray-900">
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="full-name"
+                  name="name"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="APC Student Full Name"
+                />
+              </div>
+            </div>
+
+          {/* <div>
+              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                First Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="name"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="APC Student First Name"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                Last Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="name"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="APC Student Last Name"
+                />
+              </div>
+            </div> */}
+
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                Email Address
               </label>
               <div className="mt-2">
                 <input
@@ -18,6 +105,7 @@ export default function Signup() {
                   autoComplete="email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="APC Issued Email Address"
                 />
               </div>
             </div>
@@ -27,25 +115,39 @@ export default function Signup() {
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
                   id="password"
                   name="password"
                   type="password"
-                  autoComplete="current-password"
+                  // autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Password"
                 />
               </div>
             </div>
 
             <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password-confirmation" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password Confirmation
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password-confirmation"
+                  name="password_confirmation"
+                  type="password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Password Confirmation"
+                />
+              </div>
+            </div>
+
+            {/* <div>
               <label htmlFor="contact" className="block text-sm font-medium leading-6 text-gray-900">
                 Contact Address
               </label>
@@ -59,18 +161,31 @@ export default function Signup() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
 
 
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-10 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Register
               </button>
             </div>
+
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or{" "}
+              <Link
+                to="/login"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Login with your account
+              </Link>
+      </p>
+
+
+
           </form>
         </div>
     </>
