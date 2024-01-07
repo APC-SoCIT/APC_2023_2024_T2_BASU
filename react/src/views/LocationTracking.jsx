423bddb4db1b0dd755eebc2f9853ec7452e70999
Zel <hbernante@student.apc.edu.ph>
2024-01-07 17:09:08 +0800

    Code Overhaul

--- react/src/views/LocationTracking.jsx
+++
@@ -1,9 +0,0 @@
-import PageComponent from "../components/PageComponent";
-
-export default function LocationTracking() {
-   return (
-    <PageComponent title="GPS Tracking">
-      GPS Content
-    </PageComponent>
-   );
-}
--- react/src/views/Reservation.jsx
+++
@@ -1,9 +0,0 @@
-import PageComponent from "../components/PageComponent";
-
-export default function Reservation() {
-   return (
-    <PageComponent title="Reservation">
-      Reservation Content
-    </PageComponent>
-   );
-}
