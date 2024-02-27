18604175a908bdc93287edcfee68216055aec5fc
Zel <hbernante@student.apc.edu.ph>
2024-02-27 21:31:51 +0800

    UI cont Landing Page

--- laravel-react-basu/react/src/views/LandingPage.jsx
+++ laravel-react-basu/react/src/views/LandingPage.jsx
@@ -1,13 +1,26 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { MapPinIcon, QuestionMarkCircleIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  MapPinIcon,
+  QuestionMarkCircleIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import PageComponent from "../components/PageComponent";
 import TButton from "../components/core/TButton";
-import { Navigate } from "react-router-dom";
 // import PageComponent from '../components/PageComponent'
 
 export default function LandingPage() {
   const [open, setOpen] = useState(false);
+
+  const about = [
+    { name: 'Who', description: <span><b>Client:</b> Mrs. Moana Marie C. Dingle</span> },
+    { name: 'What', description: 'A tracking and reservation system for the shuttle vehicle.' },
+    { name: 'When', description: 'Unknown Date' },
+    { name: 'Why', description: 'To provide a digitized experience on the \nShuttle Service System.' },
+    { name: 'Vision', description: 'To be Edited' },
+    { name: 'Mission', description: 'To be Edited' },
+  ]
+
 
   return (
     <PageComponent
@@ -118,6 +131,58 @@ export default function LandingPage() {
                 </a>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/*About Section*/}
+      <div className="bg-white">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              About:
+            </h2>
+            <p className="mt-4 text-gray-500">
+              This system is developed by the APPTECH team for APC's
+              Purchasing and Logistics Office Administrators.
+            </p>
+
+            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+              {about.map((feature) => (
+                <div
+                  key={feature.name}
+                  className="border-t border-gray-200 pt-4"
+                >
+                  <dt className="font-medium text-gray-900">{feature.name}</dt>
+                  <dd className="mt-2 text-sm text-gray-500">
+                    {feature.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+            <img
+              src="/images/apc-logo.png"
+              alt=""
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="/images/basu-icon-transparent.png"
+              alt=""
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="/images/apc_shuttle_bus.png"
+              alt=""
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="/images/apc-logo-2.png"
+              alt="Walnut card tray filled with cards and card angled in dedicated groove."
+              className="rounded-lg bg-gray-100"
+            />
           </div>
         </div>
       </div>
